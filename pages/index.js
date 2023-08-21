c4ca7de914fdfa3dbfb8e1db6a34e433787d0b0e
Thiago Gracini <thiago.gracini@outlook.com>
2023-08-21 11:36:00 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Emily, amo você e nossa filha, se você me ama da uma risadinha! kkk</h1>
+    return <h1>Emily, amo você e nossa filha, se você me ama da uma risadinha! kkkkk</h1>
 }
 
 export default Home;
