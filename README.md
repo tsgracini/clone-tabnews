f0e3abfbd19bc02c92740c6005846f9b20579945
Thiago Gracini <thiago.gracini@outlook.com>
2023-08-25 00:46:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto criado para aprendizado
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Emily, amo você e nossa filha, se você me ama da uma risadinha! kkkkk</h1>
+  return (
+    <h1>
+      Emily, amo você e nossa filha, se você me ama da uma risadinha! kkkkk
+    </h1>
+  );
 }
 
 export default Home;
